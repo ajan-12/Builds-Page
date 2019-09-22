3f8564ca8aaab72616f369ec7d1d76106477b963
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2019-09-23 01:21:41 +0300

    Finished the table populator.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
 function populate() {
-  var myRepos = JSON.parse(repos);
-  var insides = '<table style="width:100%" id="populate"><tr><th style="width:83%">Repository</th><th style="width:12%">Supported MC Versions</th><th style="width:5%">Build Status</th></tr>';
+  $.getJSON( "https://ajan-12.github.io/config/repos.json", function ( data ) {
+  var insides = '<caption><b style="font-size:20px">MC Spigot Plugins</b></caption><tr><th style="width:83%">Repository</th><th style="width:12%">Supported MC Versions</th><th style="width:5%">Build Status</th></tr>';
 
-  for (var i = 0; i < myRepos.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     insides += '<tr><td class="repository"><img src="icons/repository.svg" alt="" class="icon"><a href="https://github.com/';
-    insides += myRepos[i].link;
+    insides += data[i].link;
     insides += '">';
-    insides += myRepos[i].name;
+    insides += data[i].name;
     insides += '</a></td><td class="version">';
-    insides += myRepos[i].versions;
+    insides += data[i].versions;
     insides += '</td><td class="buildIcon"><img src="';
-    if (myRepos[i].status == 'success') {
+    if (data[i].status == 'success') {
       insides += 'icons/tick.svg';
     } else {
       insides += 'icons/cross.svg';
     }
     insides += '" alt="" class="icon"></td></tr>';
   }
+  
+  $("#populate").html(insides);
+  } );
 }
