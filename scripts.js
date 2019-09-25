10be6ae61e0bb0e3d429c866701a39e19c073f48
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2019-09-25 20:52:15 +0300

    Added placeholder popups.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-function populate() {
+function populateTable() {
   $.getJSON( "https://ajan-12.github.io/config/repos.json", function ( data ) {
   var insides = '<caption style="font-size: 20px; font-weight: bold;">MC Spigot Plugins</caption><tr><th style="width:83%">Repository</th><th style="width:12%">Supported MC Versions</th><th style="width:5%">Build Status</th></tr>';
 
   for (var i = 0; i < data.length; i++) {
-    insides += '<tr><td class="repository"><img src="icons/repository.svg" alt="" class="icon"><a href="https://github.com/';
-    insides += data[i].link;
-    insides += '">';
+    insides += '<tr><td class="repository"><img src="icons/repository.svg" alt="" class="icon"><button class="repo" onclick="populatePopup(\'';
     insides += data[i].name;
-    insides += '</a></td><td class="version">';
+    insides += '\');">';
+    insides += data[i].name;
+    insides += '</button></td><td class="version">';
     insides += data[i].versions;
     insides += '</td><td class="buildIcon"><img src="';
     if (data[i].status == 'success') {
@@ -20,4 +20,29 @@ function populate() {
   
   $("#populate").html(insides);
   } );
+}
+
+function populatePopup( repo ) {
+  $.getJSON("https://ajan-12.github.io/config/repos.json", function ( data ) {
+    var popup = document.getElementById("popup");
+    showPopup();
+    
+	for (var i = 0; i < data.length; i++) {
+	  if (data[i].name == repo) {
+		var body = "<p>" + data[i].description + "</p>";
+		break;
+	  }
+	}
+	
+	$("#popupHeader").html(repo);
+	$("#popupBody").html(body);
+  });
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
 }
