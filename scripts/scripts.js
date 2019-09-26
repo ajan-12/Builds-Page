e5ca6f94d07531835ad01261af8f05aed4a1e435
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2019-09-26 16:05:00 +0300

    Cleaning up the main folder.

---
+++ scripts/scripts.js
@@ -0,0 +1,48 @@
+function populateTable() {
+  $.getJSON( "https://ajan-12.github.io/config/repos.json", function ( data ) {
+  var insides = '<caption>MC Spigot Plugins</caption><tr><th>Repository</th><th>Supported MC Versions</th><th>Build Status</th></tr>';
+
+  for (var i = 0; i < data.length; i++) {
+    insides += '<tr><td class="repository"><img src="icons/repository.svg" alt="" class="icon"><button class="repo" onclick="populatePopup(\'';
+    insides += data[i].name;
+    insides += '\');">';
+    insides += data[i].name;
+    insides += '</button></td><td class="version">';
+    insides += data[i].versions;
+    insides += '</td><td class="buildIcon"><img src="';
+    if (data[i].status == 'success') {
+      insides += 'icons/tick.svg';
+    } else {
+      insides += 'icons/cross.svg';
+    }
+    insides += '" alt="" class="icon"></td></tr>';
+  }
+  
+  $("#populate").html(insides);
+  } );
+}
+
+function populatePopup( repo ) {
+  $.getJSON("https://ajan-12.github.io/config/repos.json", function ( data ) {
+    var popup = document.getElementById("popup");
+    showPopup();
+    
+	for (var i = 0; i < data.length; i++) {
+	  if (data[i].name == repo) {
+		var body = "<p>" + data[i].description + "</p>";
+		break;
+	  }
+	}
+	
+	$("#popupHeader").html(repo);
+	$("#popupBody").html(body);
+  });
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function showPopup() {
+  document.getElementById("popup").style.display = "block";
+}
